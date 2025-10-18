0549c7f3f9bc73cbbfcf9f6121ef6030a7adbb87
DESKTOP-93JG1D8\user <h1202519015@hiratagakuen.onmicrosoft.com>
2025-10-18 10:38:20 +0900

    加治：タイマー変更

--- Team_D/Assets/KAJI  AYATO/KAJI_Scripts/TimeCounter.cs
+++ Team_D/Assets/KAJI  AYATO/KAJI_Scripts/TimeCounter.cs
@@ -2,11 +2,12 @@ using System;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class TimeCounter : MonoBehaviour
 {
    
    public Text TimerText;
-    float LimitTime = 180; // 制限時間
+    float LimitTime = 180f; // 制限時間
+    private bool IsCounting = true;
 
     // Start is called before the first frame update
     void Start()
@@ -17,14 +18,34 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //カウントダウン
-        LimitTime -= Time.deltaTime;
-
-        if (LimitTime < 0)
+        if(IsCounting && LimitTime  > 0)
         {
-            LimitTime = 0;
+            LimitTime -= Time.deltaTime;
+            UpdateText();
         }
 
-        TimerText.text = LimitTime.ToString("F0"); // 残り時間を整数で表示
+        else if(LimitTime <= 0)
+        {
+            LimitTime = 0; 
+            IsCounting = false;
+            UpdateText ();
+        }
+
+        ////カウントダウン
+        //LimitTime -= Time.deltaTime;
+
+        //if (LimitTime < 0)
+        //{
+        //    LimitTime = 0;
+        //}
+
+        //TimerText.text = LimitTime.ToString("F0"); // 残り時間を整数で表示
+    }
+
+    void UpdateText()
+    {
+        int Minutes = Mathf.FloorToInt(LimitTime / 60f);
+        int Seconds = Mathf.FloorToInt(LimitTime % 60f);
+        TimerText.text = String.Format("{0:00}:{1:00}", Minutes, Seconds);
     }
 }

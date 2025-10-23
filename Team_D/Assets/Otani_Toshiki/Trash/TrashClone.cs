31e032c3f92505dfabafc4d93a3125a893088a9b
ootanitoshiki <h1202516007@hiratagakuen.onmicrosoft.com>
2025-10-23 11:04:51 +0900

    大谷：ゴミ袋追加

---
+++ Team_D/Assets/Otani_Toshiki/Trash/TrashClone.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class TrashClone : MonoBehaviour
+{
+    public GameObject player;   // 元のオブジェクトを参照する変数
+    public Transform Parent;    // 指定する親オブジェクトを参照する変数
+    float timeCount = 0f;
+    float count = 0;
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        timeCount += Time.deltaTime;
+
+        if (timeCount > 3f)
+        {
+
+
+            if (count < 10)
+            {
+                timeCount = 0;
+                // Prefab の生成
+                Instantiate(Parent, new Vector3(Random.Range(-7, 7), Random.Range(-4, 4)), Quaternion.identity);
+                player.SetActive(true); //オブジェクトを表示する
+                count++;
+            }
+            else
+            {
+                timeCount = 0;
+            }
+        }
+    }
+}

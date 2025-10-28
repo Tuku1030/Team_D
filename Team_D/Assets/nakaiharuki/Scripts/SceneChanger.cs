5430f51459bbefe68d1f7ca8f39a9f5417d59b26
ootanitoshiki <h1202516007@hiratagakuen.onmicrosoft.com>
2025-10-28 12:00:32 +0900

    レイヤー設定追加

--- Team_D/Assets/nakaiharuki/Scripts/SceneChanger.cs
+++ Team_D/Assets/nakaiharuki/Scripts/SceneChanger.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;//シーン切り替えに使える
 
+
+
 public class SceneChanger : MonoBehaviour
 {
+
     [SerializeField] private string _loadScene;//シーンの切り替え先を読み込むのに必要
 
     public void SceneChange()

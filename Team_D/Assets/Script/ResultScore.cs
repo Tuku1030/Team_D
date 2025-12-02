ec043f841a934fe852850ac1fb05d813a777c00d
ootanitoshiki <h1202516007@hiratagakuen.onmicrosoft.com>
2025-12-02 10:16:24 +0900

    大谷：変更

--- Team_D/Assets/Script/ResultScore.cs
+++ Team_D/Assets/Script/ResultScore.cs
@@ -3,27 +3,15 @@ using TMPro;
 
 public class ResultScore : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI resultScoreText; // Inspector でドラッグ
+    public TextMeshProUGUI resultScoreText;
 
 
 
     void Start()
     {
-        if (resultScoreText != null && TotalScoreManager.Instance != null)
-        {
-            // TotalScoreManager から合計スコアを取得して表示
-            int totalScore = TotalScoreManager.Instance.GetTotalScore();
-            resultScoreText.text = $"Result: {totalScore:0000}";
-            Debug.Log($"Result画面スコア表示: {totalScore}");
-            // 次のゲームのためにリセット
-            TotalScoreManager.Instance.ResetScore();
-        }
-        else
-        {
-            if (resultScoreText == null)
-                Debug.LogWarning(" ResultScoreText が Inspector にセットされていません！");
-            if (TotalScoreManager.Instance == null)
-                Debug.LogWarning(" TotalScoreManager.Instance が null です！");
-        }
+        resultScoreText.text =
+            "Result: " + TotalScoreManager.Instance.GetTotalScore();
+        // 次プレイのためにリセット
+        TotalScoreManager.Instance.ResetScore();
     }
 }

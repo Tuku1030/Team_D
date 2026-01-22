48a62afdca8e8188057e33f1af0bb2edb2db426e
ootanitoshiki <h1202516007@hiratagakuen.onmicrosoft.com>
2026-01-22 09:42:03 +0900

    大谷：上下移動のみに変更

--- Team_D/Assets/KAJI_Scripts/PlayerContller.cs
+++ Team_D/Assets/KAJI_Scripts/PlayerContller.cs
@@ -28,10 +28,8 @@ public class PlayerController : MonoBehaviour
     {
         Vector2 position = transform.position;
 
-        if (Input.GetKey(KeyCode.A)) position.x -= Speed * Time.deltaTime;
-        if (Input.GetKey(KeyCode.D)) position.x += Speed * Time.deltaTime;
-        if (Input.GetKey(KeyCode.W)) position.y += Speed * Time.deltaTime;
-        if (Input.GetKey(KeyCode.S)) position.y -= Speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.W)) position.y -= Speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.S)) position.y += Speed * Time.deltaTime;
 
         transform.position = position;
     }

2101029d2a43b4bd8adaf84360b208517939b0e6
DESKTOP-AQ3H2S0\GA4 <opopopopo0617@gmail.com>
2026-01-26 11:08:47 +0900

    大谷：上下移動修正

--- Team_D/Assets/KAJI_Scripts/PlayerContller.cs
+++ Team_D/Assets/KAJI_Scripts/PlayerContller.cs
@@ -28,8 +28,8 @@ public class PlayerController : MonoBehaviour
     {
         Vector2 position = transform.position;
 
-        if (Input.GetKey(KeyCode.W)) position.y -= Speed * Time.deltaTime;
-        if (Input.GetKey(KeyCode.S)) position.y += Speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.W)) position.y += Speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.S)) position.y -= Speed * Time.deltaTime;
 
         transform.position = position;
     }

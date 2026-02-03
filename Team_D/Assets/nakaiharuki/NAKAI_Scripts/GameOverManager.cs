72a12f0345eea66ab0fa4996404b445492646b1b
DESKTOP-AQ3H2S0\GA4 <opopopopo0617@gmail.com>
2026-02-03 14:21:38 +0900

    大谷：マージ解消

--- Team_D/Assets/nakaiharuki/NAKAI_Scripts/GameOverManager.cs
+++ Team_D/Assets/nakaiharuki/NAKAI_Scripts/GameOverManager.cs
@@ -41,6 +41,7 @@ public class GameOverManager : MonoBehaviour
 
     private IEnumerator GameOverSequence()
     {
+        //プレイヤー停止 + 演出
         GameObject player = GameObject.FindWithTag("Player");
 
         if (player != null)
@@ -48,22 +49,19 @@ public class GameOverManager : MonoBehaviour
             var pc = player.GetComponent<PlayerController>();
             if (pc != null) pc.enabled = false;
 
-            // ここで落下＋火花演出スタート
-            var fall = player.GetComponent<PlayerGameOverFall>();
-            if (fall != null) fall.StartGameOver();
+           
         }
 
-        // 演出を見る時間
-        yield return new WaitForSeconds(2f);
-
-        // フェード
+        //画面フェード
         yield return ScreenFade.Instance.StartCoroutine(
-            ScreenFade.Instance.FadeOut(1f)
+            ScreenFade.Instance.FadeOut(2f)
         );
 
+        //GameOver シーンへ
         SceneManager.LoadScene("GameOver");
     }
-        public void RestartGame()
+
+    public void RestartGame()
     {
         isGameOver = false;          //次のゲーム用にリセット
         Time.timeScale = 1f;
@@ -71,4 +69,3 @@ public class GameOverManager : MonoBehaviour
         SceneManager.LoadScene(lastStage);
     }
 }
-    
